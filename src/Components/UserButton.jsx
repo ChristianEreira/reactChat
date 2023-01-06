7e5911ad2de41324ea0dccc0eadbd058126c1fd1
Christian Ereira <contact@chrise.dev>
2023-01-06 00:03:59 +0000

    add 'selected' prop to userButton

--- src/Components/UserButton.jsx
+++ src/Components/UserButton.jsx
@@ -2,13 +2,13 @@ import React from "react";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-const UserButton = ({ userid, title, subtext, avatarColor, avatarContent }) => {
+const UserButton = ({ userid, title, subtext, avatarColor, avatarContent, selected }) => {
     // TODO: Add icon support for 'online users' chat button
     if (userid) {
         // TODO: Set variables based on userid
     }
     return (
-        <div className="userButton noIcon hoverable selected">
+        <div className={`userButton noIcon hoverable ${selected && "selected"}`}>
             <div className={`avatar ${avatarColor}`}>
                 {avatarContent}
             </div>
