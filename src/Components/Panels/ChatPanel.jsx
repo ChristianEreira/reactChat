1c62ea8c95ced396295d42668ef3c2328e404019
Christian Ereira <contact@chrise.dev>
2023-01-06 13:35:36 +0000

    fix element ordering

--- src/Components/Panels/ChatPanel.jsx
+++ src/Components/Panels/ChatPanel.jsx
@@ -11,13 +11,13 @@ const ChatPanel = () => {
             <div id="chatBottom">
                 <div id="messages">
                     <p className="emptyMessage"><i>There are no messages yet. Say hi!</i></p>
-                    <p id="charCount">{currentCharacters}/300</p>
-                    {/* TODO: onSubmit */}
-                    <form className="centerX" id="msgInput" onSubmit={() => { }}>
-                        <textarea name="msg" id="msgBox" placeholder="Type a message..." maxLength="300" onInput={(e) => { setCurrentCharacters(e.target.value.length) }}></textarea>
-                        <button type="submit" id="sendBtn"><FontAwesomeIcon icon={solid('paper-plane')} /></button>
-                    </form>
                 </div>
+                <p id="charCount">{currentCharacters}/300</p>
+                {/* TODO: onSubmit */}
+                <form className="centerX" id="msgInput" onSubmit={() => { }}>
+                    <textarea name="msg" id="msgBox" placeholder="Type a message..." maxLength="300" onInput={(e) => { setCurrentCharacters(e.target.value.length) }}></textarea>
+                    <button type="submit" id="sendBtn"><FontAwesomeIcon icon={solid('paper-plane')} /></button>
+                </form>
             </div>
         </div>
     );
