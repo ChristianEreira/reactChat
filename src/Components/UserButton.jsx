42707a6855af6935f4a2c802b23bc4ec0d0bdd89
Christian Ereira <contact@chrise.dev>
2023-01-06 00:06:24 +0000

    add 'hoverable' prop to UserButton

--- src/Components/UserButton.jsx
+++ src/Components/UserButton.jsx
@@ -2,13 +2,13 @@ import React from "react";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-const UserButton = ({ userid, title, subtext, avatarColor, avatarContent, selected }) => {
+const UserButton = ({ userid, title, subtext, avatarColor, avatarContent, selected, hoverable }) => {
     // TODO: Add icon support for 'online users' chat button
     if (userid) {
         // TODO: Set variables based on userid
     }
     return (
-        <div className={`userButton noIcon hoverable ${selected && "selected"}`}>
+        <div className={`userButton noIcon ${hoverable && "hoverable"} ${selected && "selected"}`}>
             <div className={`avatar ${avatarColor}`}>
                 {avatarContent}
             </div>
