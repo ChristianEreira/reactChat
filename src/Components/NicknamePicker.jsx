13432f8674c713b9be2e48fb3800fc26f6938627
Christian Ereira <contact@chrise.dev>
2023-01-07 12:23:41 +0000

    add nickname validation

--- src/Components/NicknamePicker.jsx
+++ src/Components/NicknamePicker.jsx
@@ -1,8 +1,25 @@
-const NicknamePicker = ({closePopup}) => {
+import { useState } from 'react';
+
+const NicknamePicker = ({ closePopup }) => {
+    const [nickError, setNickError] = useState(null);
+
+    const checkNick = e => {
+        let nick = e.target.value;
+        if (nick.length > 20) {
+            setNickError("Nickname must be less than 20 characters");
+        } else if (nick === "") {
+            setNickError("Nickname cannot be blank");
+        } else if (!nick.match(/^[0-9a-z]+$/i)) {
+            setNickError("Nickname must only use a-z and 0-9");
+        } else {
+            setNickError(null);
+        }
+    };
+
     return (
         <form>
-            <input type="text" id="nickInput" placeholder="Nickname" />
-            <p id="nickError" className="invisible">Error</p>
+            <input type="text" id="nickInput" className={nickError && "inputError"} placeholder="Nickname" onInput={checkNick} />
+            <p id="nickError" className={!nickError && "invisible"}>{nickError}</p>
             <button type="submit" className="button primary">Confirm</button>
             <button type="button" className="button secondary" onClick={closePopup}>Cancel</button>
         </form>
