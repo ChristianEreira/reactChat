fcedd49a68d709a3ea03bca1e7d7669909a25d83
Christian Ereira <contact@chrise.dev>
2023-01-12 16:27:51 +0000

    add onClick events

--- src/Components/InfoBar.jsx
+++ src/Components/InfoBar.jsx
@@ -1,14 +1,14 @@
 const InfoBar = ({leftIcon, rightIcon, leftOnClick, rightOnClick, title, avatarColor, avatarContent}) => {
     return (
         <div id="infoBar">
-            <div>
+            <div onClick={leftOnClick}>
                 {leftIcon}
             </div>
             <div className="title">
                 <div className={`avatar ${avatarColor} small`}>{avatarContent}</div>
                 <p className="bold">{title}</p>
             </div>
-            <div>
+            <div onClick={rightOnClick}>
                 {rightIcon}
             </div>
         </div>
