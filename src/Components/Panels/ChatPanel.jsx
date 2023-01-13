2eca0d648ac5a19dd7283ad9f95d581f006d2a1a
Christian Ereira <contact@chrise.dev>
2023-01-13 15:49:41 +0000

    implement message sending

--- src/Components/Panels/ChatPanel.jsx
+++ src/Components/Panels/ChatPanel.jsx
@@ -3,22 +3,34 @@ import ChatMessage from "../ChatMessage";
 import InfoBar from "../InfoBar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { sanitize } from "../../helpers";
 
-const ChatPanel = ({ nicks, activeChat }) => {
-    const [currentCharacters, setCurrentCharacters] = useState(0);
+const ChatPanel = ({ nicks, activeChat, socket }) => {
+    const [currentMessage, setCurrentMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let message = currentMessage.trim();
+
+        if (message !== "") {
+            socket.emit("chat message", sanitize(message), activeChat);
+
+            // TODO: Call add message function
+            setCurrentMessage("");
+        }
+    };
 
     return (
         <div id="chatPanel">
-            {activeChat !== "global" && <InfoBar title={nicks[activeChat].nick} avatarColor={nicks[activeChat].color} avatarContent={nicks[activeChat].nick} rightIcon=<FontAwesomeIcon icon={solid("trash")} /> />}
+            {activeChat !== "global" && <InfoBar title={nicks[activeChat].nick} avatarColor={nicks[activeChat].color} avatarContent={nicks[activeChat].nick} rightIcon=<FontAwesomeIcon icon={solid("trash")} /> rightOnClick={() => { alert(activeChat) }} />}
             <div id="chatBottom">
                 <div id="messages">
                     <ChatMessage message={{ msg: ["This is the first message!", "and this is the sencond"] }} />
                     <p className="emptyMessage"><i>There are no messages yet. Say hi!</i></p>
                 </div>
-                <p id="charCount">{currentCharacters}/300</p>
-                {/* TODO: onSubmit */}
-                <form className="centerX" id="msgInput" onSubmit={() => { }}>
-                    <textarea name="msg" id="msgBox" placeholder="Type a message..." maxLength="300" onInput={(e) => { setCurrentCharacters(e.target.value.length) }}></textarea>
+                <p id="charCount">{currentMessage.trim().length}/300</p>
+                <form className="centerX" id="msgInput" onSubmit={handleSubmit}>
+                    <textarea name="msg" id="msgBox" placeholder="Type a message..." maxLength="300" value={currentMessage} onInput={(e) => { setCurrentMessage(e.target.value) }}></textarea>
                     <button type="submit" id="sendBtn"><FontAwesomeIcon icon={solid('paper-plane')} /></button>
                 </form>
             </div>
