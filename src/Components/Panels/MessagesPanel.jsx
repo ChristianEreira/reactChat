3f70ac7c408018a06d153794d334bbc696a4ae58
Christian Ereira <contact@chrise.dev>
2023-01-16 17:03:33 +0000

    fix disconnected message list display

--- src/Components/Panels/MessagesPanel.jsx
+++ src/Components/Panels/MessagesPanel.jsx
@@ -4,14 +4,20 @@ import UserButton from "../UserButton";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-const MessagesPanel = ({openChat, messages, getUserInfo, activeChat, unreadChats}) => {
+const MessagesPanel = ({ openChat, messages, getUserInfo, activeChat, unreadChats }) => {
     const [searchTerm, setSearchTerm] = useState('');
 
     let messagesList;
     if (Object.keys(messages).length > 1) {
-        messagesList = Object.entries(messages).filter(([id, msgs]) => id !== "global" && getUserInfo(id).nick.toLowerCase().includes(searchTerm.toLowerCase())).map(([id, msgs]) => {
+        messagesList = Object.entries(messages).filter(([id, msgs]) => {
+            if (searchTerm === '') {
+                return id !== "global";
+            } else {
+                return id !== "global" && !getUserInfo(id).disconnected && getUserInfo(id).nick.toLowerCase().includes(searchTerm.toLowerCase());
+            }
+        }).map(([id, msgs]) => {
             let user = getUserInfo(id);
-            return <UserButton avatarColor={user.color} avatarContent={user.nick} title={user.nick} subtext={messages[id].length > 0 ? messages[id][messages[id].length - 1].msg[messages[id][messages[id].length - 1].msg.length - 1] : undefined} unread={unreadChats.has(id)} selected={id === activeChat} hoverable onClick={() => {openChat(id)}} key={id} />;
+            return <UserButton avatarColor={user.color} avatarContent={user.nick} title={user.nick} subtext={messages[id].length > 0 ? messages[id][messages[id].length - 1].msg[messages[id][messages[id].length - 1].msg.length - 1] : undefined} unread={unreadChats.has(id)} selected={id === activeChat} hoverable onClick={() => { openChat(id) }} key={id} />;
         });
     } else {
         messagesList = <p className="emptyMessage"><i>Click on a user to start a chat</i></p>;
@@ -21,7 +27,7 @@ const MessagesPanel = ({openChat, messages, getUserInfo, activeChat, unreadChats
         <div id="messagesMenu">
             <h1>Messages</h1>
             <SearchBar setSearchTerm={setSearchTerm} />
-            <UserButton avatarColor="grey" avatarContent={<FontAwesomeIcon icon={solid('users')} />} title="Global chat" subtext="Chat with all users" unread={unreadChats.has("global")} selected={activeChat === "global"} hoverable onClick={() => {openChat('global')}} />
+            <UserButton avatarColor="grey" avatarContent={<FontAwesomeIcon icon={solid('users')} />} title="Global chat" subtext="Chat with all users" unread={unreadChats.has("global")} selected={activeChat === "global"} hoverable onClick={() => { openChat('global') }} />
             <hr className="seperator" />
             <div id="messagesList">
                 {messagesList}
