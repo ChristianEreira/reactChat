0a670c1f85de2eb3dcfc8be20afa1b9efafd621f
Christian Ereira <contact@chrise.dev>
2023-01-21 16:07:58 +0000

    hide title if not given

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -2,7 +2,7 @@ const Popup = ({ children, isOpen, title }) => {
     return (
         <div className={`center popupBack ${!isOpen && "hidden"}`}>
             <div className="box">
-                <h1>{title}</h1>
+                {title && <h1>{title}</h1>}
                 {children}
             </div>
         </div>
