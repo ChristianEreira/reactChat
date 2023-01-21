7d3de8c6276edf3039bc9488003b9e4eec867bbc
Christian Ereira <contact@chrise.dev>
2023-01-21 16:07:46 +0000

    hide avatar if not given content

--- src/Components/InfoBar.jsx
+++ src/Components/InfoBar.jsx
@@ -1,12 +1,12 @@
-const InfoBar = ({leftIcon, rightIcon, leftOnClick, rightOnClick, title, avatarColor, avatarContent}) => {
+const InfoBar = ({leftIcon, rightIcon, leftOnClick, rightOnClick, title, avatarColor, avatarContent, margin}) => {
     return (
-        <div id="infoBar">
+        <div id="infoBar" className={margin ? "withMargin" : undefined}>
             <div onClick={leftOnClick}>
                 {leftIcon}
             </div>
             <div className="title">
-                <div className={`avatar ${avatarColor} small`}>{avatarContent}</div>
-                <p className="bold">{title}</p>
+                {avatarContent && <div className={`avatar ${avatarColor} small`}>{avatarContent}</div>}
+                <p className={`bold ${!avatarContent && "noMargin"}`}>{title}</p>
             </div>
             <div onClick={rightOnClick}>
                 {rightIcon}
