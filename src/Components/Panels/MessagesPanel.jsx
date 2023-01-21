030a610ae2558518b5b2855523aa915638cddebc
Christian Ereira <contact@chrise.dev>
2023-01-21 14:50:05 +0000

    show new chat button on medium view

--- src/Components/Panels/MessagesPanel.jsx
+++ src/Components/Panels/MessagesPanel.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AppSizeContext } from "../../App";
 import SearchBar from "../SearchBar";
 import UserButton from "../UserButton";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-const MessagesPanel = ({ openChat, messages, getUserInfo, activeChat, unreadChats }) => {
+const MessagesPanel = ({ openChat, messages, getUserInfo, activeChat, unreadChats, handleNewChatClick }) => {
     const [searchTerm, setSearchTerm] = useState('');
+    const appSize = useContext(AppSizeContext);
 
     let messagesList;
     if (Object.keys(messages).length > 1) {
@@ -26,6 +28,7 @@ const MessagesPanel = ({ openChat, messages, getUserInfo, activeChat, unreadChat
     return (
         <div id="messagesMenu">
             <h1>Messages</h1>
+            {appSize !== "large" && <div className="button primary full" onClick={handleNewChatClick}>Message a new user</div>}
             <SearchBar setSearchTerm={setSearchTerm} />
             <UserButton avatarColor="grey" avatarContent={<FontAwesomeIcon icon={solid('users')} />} title="Global chat" subtext="Chat with all users" unread={unreadChats.has("global")} selected={activeChat === "global"} hoverable onClick={() => { openChat('global') }} />
             <hr className="seperator" />
