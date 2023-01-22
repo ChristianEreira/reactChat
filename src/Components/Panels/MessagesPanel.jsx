1a6efa945a8c0ae8eece3f3effa38d3ff28524c7
Christian Ereira <contact@chrise.dev>
2023-01-22 00:49:59 +0000

    hide active chat on small view

--- src/Components/Panels/MessagesPanel.jsx
+++ src/Components/Panels/MessagesPanel.jsx
@@ -19,7 +19,7 @@ const MessagesPanel = ({ openChat, messages, getUserInfo, activeChat, unreadChat
             }
         }).map(([id, msgs]) => {
             let user = getUserInfo(id);
-            return <UserButton avatarColor={user.color} avatarContent={user.nick} title={user.nick} subtext={messages[id].length > 0 ? messages[id][messages[id].length - 1].msg[messages[id][messages[id].length - 1].msg.length - 1] : undefined} unread={unreadChats.has(id)} selected={id === activeChat} hoverable onClick={() => { openChat(id) }} key={id} />;
+            return <UserButton avatarColor={user.color} avatarContent={user.nick} title={user.nick} subtext={messages[id].length > 0 ? messages[id][messages[id].length - 1].msg[messages[id][messages[id].length - 1].msg.length - 1] : undefined} unread={unreadChats.has(id)} selected={id === activeChat && appSize !== "small"} hoverable onClick={() => { openChat(id) }} key={id} />;
         });
     } else {
         messagesList = <p className="emptyMessage"><i>Click on a user to start a chat</i></p>;
@@ -30,7 +30,7 @@ const MessagesPanel = ({ openChat, messages, getUserInfo, activeChat, unreadChat
             <h1>Messages</h1>
             {appSize !== "large" && <div className="button primary full" onClick={handleNewChatClick}>Message a new user</div>}
             <SearchBar setSearchTerm={setSearchTerm} />
-            <UserButton avatarColor="grey" avatarContent={<FontAwesomeIcon icon={solid('users')} />} title="Global chat" subtext="Chat with all users" unread={unreadChats.has("global")} selected={activeChat === "global"} hoverable onClick={() => { openChat('global') }} />
+            <UserButton avatarColor="grey" avatarContent={<FontAwesomeIcon icon={solid('users')} />} title="Global chat" subtext="Chat with all users" unread={unreadChats.has("global")} selected={activeChat === "global" && appSize !== "small"} hoverable onClick={() => { openChat('global') }} />
             <hr className="seperator" />
             <div id="messagesList">
                 {messagesList}
