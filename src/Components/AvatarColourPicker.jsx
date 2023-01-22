94e3990cc704c3a999b6293f155b28b14260b65b
Christian Ereira <contact@chrise.dev>
2023-01-22 18:25:39 +0000

    add 'big' styling

--- src/Components/AvatarColourPicker.jsx
+++ src/Components/AvatarColourPicker.jsx
@@ -1,11 +1,11 @@
-const AvatarColourPicker = ({selected, socket}) => {
+const AvatarColourPicker = ({selected, socket, big}) => {
     const colours = ["red", "yellow", "green", "cyan", "blue"];
 
     return (
         <div className="userButton noIcon">
             <div className="userDesc">
-                <p><b>Avatar colour:</b></p>
-                <div className="colorSelectList">
+                <p className={big ? "mobileHeading noMargin" : undefined}><b>Avatar colour:</b></p>
+                <div className={`colorSelectList ${big && "noMargin"}`}>
                     {colours.map((colour) => {
                         return (
                             <div className={`colorSelect ${colour} ${colour === selected && "selected"}`} onClick={() => {socket.emit('set color', colour)}} key={colour}></div>
